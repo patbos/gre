109b82c6bcdd0f7c54379f4be8cb49f71fa0199c
Patrik Boström <patbos@patbos.com>
2012-07-05 22:28:37 +0200

    Made assignment static to make it easier to refactor

--- src/main/groovy/com/patbos/gre/GreCategory.groovy
+++ src/main/groovy/com/patbos/gre/GreCategory.groovy
@@ -7,34 +7,34 @@ class GreCategory {
     }
 
     def static exec(Script script, String command) {
-        def greRuntime = script.binding.getVariable("gre")
+        GreRuntime greRuntime = (GreRuntime) script.binding.getVariable("gre")
         greRuntime.exec(command)
     }
 
     def static exec(Script script, String command, boolean throwException) {
-        def greRuntime = script.binding.getVariable("gre")
+        GreRuntime greRuntime = (GreRuntime) script.binding.getVariable("gre")
         greRuntime.exec(command, throwException, true, false, null)
     }
 
 
     def static exec(Script script, String command, boolean throwException, boolean log) {
-        def greRuntime = script.binding.getVariable("gre")
+        GreRuntime greRuntime = (GreRuntime) script.binding.getVariable("gre")
         greRuntime.exec(command, throwException, log, false, null)
     }
 
     def static sudoExec(Script script, String command, String password) {
-        def greRuntime = script.binding.getVariable("gre")
+        GreRuntime greRuntime = (GreRuntime) script.binding.getVariable("gre")
         greRuntime.sudoExec(command, password)
     }
 
 
     def static put(Script script, File file, String destination) {
-        def greRuntime = script.binding.getVariable("gre")
+        GreRuntime greRuntime = (GreRuntime) script.binding.getVariable("gre")
         greRuntime.put(file, destination)
     }
 
     def static println(Script script, Object output) {
-        def greRuntime = script.binding.getVariable("gre")
+        GreRuntime greRuntime = (GreRuntime) script.binding.getVariable("gre")
         greRuntime.logScript(output)
     }
 
