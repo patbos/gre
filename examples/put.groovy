ce4cef9847a0f9820d7f259560fe6b773162b4ab
Patrik Boström <patrik.bostrom@UNIBET.com>
2012-07-05 10:30:27 +0200

    Fixed example

--- examples/put.groovy
+++ examples/put.groovy
@@ -1,6 +1,1 @@
-//put(new File("/home/patbos/Downloads/grails-2.0.4.zip"), "/tmp/grails")
-//exec("ls -la /tmp/grailss", true)
-//exec("rm /tmp/grails")
-
-
-exec("wget -q http://release.unibet.com/nexus/content/repositories/pipeline/com/unibet/boat/boatbo-deployment-package/1.0.2.8/boatbo-deployment-package-1.0.2.8.jar")
+put(new File("/tmp/file"), "/tmp/file")
